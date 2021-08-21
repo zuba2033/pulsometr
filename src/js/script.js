583f4c88d77cfb99336e04575b2d99e74382292f
zuba2033 <saitman9999@gmail.com>
2021-08-21 09:39:55 +0300

    near finish creating modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,41 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 300,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/4th_scr/chevron_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/4th_scr/chevron_right.svg"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false
+              }
+            },
+          ]
+        
+    });
 
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 
+   
 
+    function toggleSlide(item) {
+      $(item).each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+  
+        })
+      })
+    };
 
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 
-let age = prompt("Сколько Вам лет?, '' ")    
-let message;
-
-if (age !>= 14 && age !<= 90) {
-    message = 'Вы проходите';
-};
-
-alert( message )
+    });
