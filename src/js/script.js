2c2b1411a54b59fb9e8fc96d37dcffe08fea9382
zuba2033 <saitman9999@gmail.com>
2021-08-29 23:02:32 +0300

    start animations

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,7 @@
-$(document).ready(function(){
+
+
+
+$(document).ready(function() {
     $('.carousel__inner').slick({
         speed: 300,
         adaptiveHeight: true,
@@ -41,22 +44,85 @@ $(document).ready(function(){
 
 // modal
 
-$('[data-modal=consultation').on('click', function() {
-  $('.overlay, #consultation').fadeIn('slow');
-});
+  $('[data-modal=consultation').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
 
-$('.modal__close').on('click', function() {
-  $('.overlay, #consultation, #reply, #order').fadeOut('slow');
-});
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #reply, #order').fadeOut('slow');
+  });
 
-$('.button_catalog-item').on('click', function() {
-  $('.overlay, #order').fadeIn('slow');
-});
+  $('.button_catalog-item').on('click', function() {
+    $('.overlay, #order').fadeIn('slow');
+  });
 
-$('.button_catalog-item').each(function(i) {
-  $(this).on('click', function() {
-    $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-  })
-});
+  $('.button_catalog-item').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+    })
+  });
+
+  // validation
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: "Пожалуйста, введите свое имя",
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свой e-mail",
+          email: "Адрес e-mail должен содержать символ @"
+        }
+      }
     });
+  };
+  
+  validateForms ('#consultation form');
+  validateForms ('#consultation-form');
+  validateForms ('#order form');
+
+  $('input[name=phone]').mask("(999) 999-99-99");
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #reply').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  // pageup
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+ //smooth scroll 
+
+  $("a[href^='#']").click(function() {
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+});
 
